c3719cb041d6a918676fc6f2a77ef8cc3b132728
SivaMaharajaG <sivamaharaja777@gmail.com>
2025-08-02 20:12:17 +0530

    Update login.py

--- auth/login.py
+++ auth/login.py
@@ -22,7 +22,4 @@ def show_login():
         else:
             st.error("Invalid credentials")
         conn.close()
-if st.session_state.role == "user":
-    show_user_page()
-elif st.session_state.role == "admin":
-    show_admin_page()
+
